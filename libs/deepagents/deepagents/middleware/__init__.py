dea10ad81790c47a3d6a43d6accc4eb436123dc9
Zvi Lapp <lappzvi@gmail.com>
2026-01-01 18:59:34 +0200

    Add SkillsMiddleware export to deepagents.middleware.__init__.py

--- libs/deepagents/deepagents/middleware/__init__.py
+++ libs/deepagents/deepagents/middleware/__init__.py
@@ -3,9 +3,15 @@
 from deepagents.middleware.filesystem import FilesystemMiddleware
 from deepagents.middleware.subagents import CompiledSubAgent, SubAgent, SubAgentMiddleware
 
+try:
+    from deepagents_cli.skills.middleware import SkillsMiddleware
+except ImportError:
+    SkillsMiddleware = None  # type: ignore[assignment, misc]
+
 __all__ = [
     "CompiledSubAgent",
     "FilesystemMiddleware",
+    "SkillsMiddleware",
     "SubAgent",
     "SubAgentMiddleware",
 ]
